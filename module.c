f6d0f9ac8be01146f9f1570beebcdf3e5817a4b1
Tom Hanks <tommy.hanks@gmail.com>
2018-12-27 05:55:20 -0500

    Initial commit.

---
+++ module.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include "redismodule.h"
+#include "rmutil/strings.h"
+
+int DateTimeInfo(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    const char *sargs[argc];
+    RMUtil_StringConvert(argv, sargs, argc, RMUTIL_STRINGCONVERT_COPY);
+
+    time_t parsedSeconds;
+    sscanf(sargs[1], "%ld", &parsedSeconds);
+
+    struct tm *parsedTime;
+    parsedTime = localtime(&parsedSeconds);
+
+    int weekOfYear = (int)ceil((double)parsedTime->tm_yday / 7.0f);
+
+    RedisModule_ReplyWithArray(ctx, 2);
+
+    char *stringWeekOfYearLabel = "week_of_year";
+    RedisModuleString *weekOfYearLabel = RedisModule_CreateString(ctx, stringWeekOfYearLabel, sizeof(char) * 12);
+    RedisModule_ReplyWithString(ctx, weekOfYearLabel);
+
+    RedisModuleString *weekOfYearValue = RedisModule_CreateStringPrintf(ctx, "%d", weekOfYear);
+    RedisModule_ReplyWithString(ctx, weekOfYearValue);
+
+    RedisModule_Free(weekOfYearLabel);
+    RedisModule_Free(weekOfYearValue);
+
+    return REDISMODULE_OK;
+}
+
+int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
+{
+    if (RedisModule_Init(ctx, "datetime", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR)
+    {
+        return REDISMODULE_ERR;
+    }
+
+    if (RedisModule_CreateCommand(ctx, "datetime.info", DateTimeInfo, "readonly", 1, 1, 1) == REDISMODULE_ERR)
+    {
+        return REDISMODULE_ERR;
+    }
+
+    return REDISMODULE_OK;
+}
